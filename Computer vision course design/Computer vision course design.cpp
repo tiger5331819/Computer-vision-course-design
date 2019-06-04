216abb164da34e7dc70d216ab5df013abaf4614a
suhuyuan <tiger5331819@163.com>
2019-06-04 08:52:06 +0800

    添加项目文件。

---
+++ Computer vision course design/Computer vision course design.cpp
@@ -0,0 +1,13 @@
+﻿#include"Head.h"
+using namespace std;
+using namespace cv;
+
+int main()
+{
+	Mat image = imread("1.jpg");
+	imshow("test", image);
+	waitKey(0);
+    cout << "Hello World!\n";
+}
+
+
---
+++ Computer vision course design/Head.h
@@ -0,0 +1,9 @@
+#pragma once
+#ifndef HEAD
+#define HEAD
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+
+#endif // HEAD
